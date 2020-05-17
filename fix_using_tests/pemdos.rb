9336edb14ae8b5c82cb59ddaedf41f80cefb318e
Esther Kang <estherkang14@gmail.com>
2020-05-17 05:28:35 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
 
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    ten_s = "ssssssssss"
+    "#{ten_s}#{string}"
   else
-    string
+    "#{string}"
   end
 end
+
+snake_it_up("surprise")
